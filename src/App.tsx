09738347d220e63c95a3b3c29655733b77732e1a
kazuki-takane <119714353+kazuki-takane@users.noreply.github.com>
2023-05-11 20:26:41 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Practice1 } from "./practices/Practice1";
+import { Practice2 } from "./practices/Practice2";
+import { Practice3 } from "./practices/Practice3";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Practice1 />
+      <Practice2 />
+      <Practice3 />
+    </div>
+  );
+}
